ee48e394e4d82e6291a2dc0208db3e77add8901a
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2024-09-24 11:10:48 +0300

    day 2 sql

--- ders1.sql
+++ ders1.sql
@@ -112,9 +112,84 @@ select date_part('year',o.order_date) as "Yıl", date_part('month',o.order_date)
 from order_details od
          inner join orders o
                     on od.order_id = o.order_id
-group by date_part('year', o.order_date),date_part('month', o.order_date)
+group by date_part('year', o.order_date), date_part('month', o.order_date)
 having SUM(od.quantity * od.unit_price) > 100000
 order by "Toplam Satış" desc
 
 
 -- recursive query
+select * from order_details od
+
+select min(unit_price) from order_details od -- => 2
+
+-- Minimum fiyata sahip siparişler
+select * from order_details od where unit_price = ( select min(unit_price) from order_details od )
+
+-- sql teknolojileri -> limit, TOP 3
+select * from products
+where unit_price = (select max(unit_price) from products p)
+    limit 4
+
+-- SAYFALAMA
+-- offset 0 limit 10 -> 1. sayfadaki 10 veri
+-- offset 10 limit 10 -> 2. sayfadaki 10 veri
+-- offset (bulunan sayfa - 1 * limit), limit (sayfa başı gösterilecek veri)
+select * from products p limit 10 offset 30
+
+select count(*)/10 from products
+
+
+-- alt sorgu kullanılacak tamamında
+select * from order_details od where product_id = 60
+-- en çok sipariş verilen ürün detayları (ismini görsek yeterli).
+select * from products p where product_id =
+                               (select product_id from order_details od
+                                group by product_id
+                                order by SUM(quantity) desc
+    limit 1)
+
+-- ismi "Beverages" olan kategoride en ucuz ürün fiyatı.
+select * from products
+where category_id
+          = (select category_id from categories c where c.category_name = 'Beverages')
+order by unit_price
+    limit 1
+
+-- 10.45 -> Update,Create,Delete - SQL Injection
+select * from categories c
+
+-- Insert
+    insert into categories(category_name, category_id, description) VALUES( 'Deneme 2', 10, 'Açıklama 2')
+
+-- Bir kıstas belirtilmez ise, tüm veriler etkilenir.
+-- Update
+update categories c
+set category_name = 'Deneme 4'
+where c.category_id = 10
+
+-- Delete
+delete from categories c where c.category_id=10
+
+-- CRUD Operasyonları (Create,Read,Update,Delete)
+
+-- SQL Injection 101
+
+
+    insert into users(username,password) values('kodlamaio','kodlamaio')
+select * from users
+
+select * from users where username='etiya' and password='etiya1'
+
+select * from users where username='etiya' and password='etiya1' or 1=1; exec xp_cmdshell('whoami');
+
+-- NTUI -> Never Trust User Input
+select * from users where id = 1 or 1=1
+
+-- input= 'etiya1' or 1=1
+--
+select * from information_schema.columns  where table_name='users'
+
+-- username,password
+
+
+
